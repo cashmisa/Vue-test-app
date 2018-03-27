396583fcf0700251bee497ae3d330cf1ef46f500
Xiaowen Tang <Xiaowen@Xiaowens-MacBook-Pro.local>
2018-03-28 03:01:12 +0800

    add percentage

--- src/components/auth/signup.vue
+++ src/components/auth/signup.vue
@@ -10,29 +10,34 @@
           <!--$v gives access to validators we set up-->
           <!--$touch is a method  -->
           <!--<div>{{ $v }}</div>-->
-
+          <div v-if="$v.email.$error">
           <!--if the email: false as it is not valid email, then show this-->
           <small v-if="!$v.email.email" class="text-danger">Invalid email address</small>
-          <small v-if="!$v.email.required && $v.email.$error" class="text-danger">Email is required</small>
-          <small v-if="!$v.email.unique && $v.email.$error" class="text-danger">Email is taken</small>
+          <small v-if="!$v.email.required" class="text-danger">Email is required</small>
+          <small v-if="!$v.email.unique" class="text-danger">Email is taken</small>
+        </div>
         </div>
         <div class="input" :class="{ invalid: $v.age.$error}">
           <label for="age">Your Age</label>
           <input type="number" id="age" v-model="age" @blur="$v.age.$touch()">
-          <small v-if="!$v.age.minValue" class="text-danger">Must be at least {{ $v.age.$params.minValue.min}}</small>
-          <small v-if="!$v.age.required && $v.age.$error" class="text-danger">Age is required</small>
+         <div v-if="$v.age.$error">
+          <small v-if="!$v.age.minValue || !$v.age.numeric" class="text-danger">Must be at least {{ $v.age.$params.minValue.min}}</small>
+          <small v-if="!$v.age.required" class="text-danger">Age is required</small>
+          </div>
         </div>
         <div class="input" :class="{ invalid: $v.password.$error}">
           <label for="password">Password</label>
           <input type="password" id="password" v-model="password" @blur="$v.password.$touch()">
+          <div v-if="$v.password.$error">
           <small v-if="!$v.password.minLength" class="text-danger">Must be at least {{ $v.password.$params.minLength.min }} characters</small>
-          <small v-if="!$v.password.required && $v.password.$error" class="text-danger">Password is required</small>
-        </div>
+          <small v-if="!$v.password.required" class="text-danger">Password is required</small>
+        </div></div>
         <div class="input" :class="{ invalid: $v.confirmPassword.$error}">
           <label for="confirm-pw">Confirm Password</label>
           <input type="password" id="confirm-pw" v-model="confirmPassword" @blur="$v.confirmPassword.$touch()">
+          <div v-if="$v.confirmPassword.$error">
           <small v-if="!$v.confirmPassword.sameAsPassword" class="text-danger">Passwords must be identical</small>
-        </div>
+        </div></div>
         <div class="input">
           <label for="country">Country</label>
           <select name="country" id="country" v-model="country">
@@ -67,14 +72,15 @@
                 <button
                   @click.prevent="onDeleteHobby(hobby.id)">X
                 </button>
+                <div v-if="$v.hobbyInputs.$each[index].value.$error">
                 <small
-                  v-if="!$v.hobbyInputs.$each[index].value.required && $v.hobbyInputs.$each[index].value.$error"
+                  v-if="!$v.hobbyInputs.$each[index].value.required"
                   class="text-danger">required</small>
                 <small
                   v-if="!$v.hobbyInputs.$each[index].value.minLength"
                   class="text-danger">Must contain at least
                   {{ $v.hobbyInputs.$each[index].value.$params.minLength.min}}
-                  characters</small>
+                  characters</small></div>
               </div>
             </div>
           </div>
@@ -86,208 +92,226 @@
                  v-model="terms">
           <label for="terms">Accept Terms of Use (not required for China)</label>
         </div>
+        <hr>
+        <Percentage @percentageValidated = "percentageValidated=true"></Percentage>
+        <hr>
         <div class="submit">
-          <button type="submit" :disabled="$v.$invalid">Submit</button>
+          <button type="submit" :disabled="$v.$invalid || !percentageValidated">Submit</button>
         </div>
       </form>
     </div>
   </div>
 </template>
 <script>
-  import axios from 'axios'
-import { required, email, numeric, minValue, sameAs, minLength, requiredUnless} from 'vuelidate/lib/validators'
-  export default {
-    name: 'Signup',
-    data () {
-      return {
-        email: '',
-        age: null,
-        password: '',
-        confirmPassword: '',
-        country: 'usa',
-        hobbyInputs: [],
-        terms: false
+import axios from "axios";
+import Percentage from '../fields/percentage.vue'
+import {
+  required,
+  email,
+  numeric,
+  minValue,
+  sameAs,
+  minLength,
+  requiredUnless
+} from "vuelidate/lib/validators";
+export default {
+  name: "Signup",
+  components: { Percentage },
+  data() {
+    return {
+      email: "",
+      age: null,
+      password: "",
+      confirmPassword: "",
+      country: "usa",
+      hobbyInputs: [],
+      terms: false,
+      percentageValidated: false,
+    };
+  },
+  validations: {
+    email: {
+      //custom name : imported validator - required: required,
+      required,
+      email,
+      unique: inputValue => {
+        //return true or false to determine if (true)valid or (false)not valid
+        //e.g. return false - always invalid
+        if (inputValue === "") return true; //this validator doesn't care about other criteria
+        //here it is firebase specific (use query string and orderby)
+        return axios
+          .get('/users.json?orderBy="email"&equalTo="' + inputValue + '"')
+          .then(res => {
+            return Object.keys(res.data).length === 0;
+          })
+          .catch(err => console.log(err));
       }
     },
-    validations:{
-      email: {
-          //custom name : imported validator - required: required,
-        required,
-        email,
-        unique: inputValue => {
-          //return true or false to determine if (true)valid or (false)not valid
-          //e.g. return false - always invalid
-          if (inputValue === '') return true; //this validator doesn't care about other criteria
-          //here it is firebase specific (use query string and orderby)
-          return axios.get('/users.json?orderBy="email"&equalTo="' + inputValue + '"')
-            .then(res=>{
-              return Object.keys(res.data).length === 0
-            }).catch(err=>console.log(err))
-
-        }
-      },
-      age:{
-        required,
-        numeric,
-        minValue: minValue(18)
-        // between: between(20, 30)
-      },
-      password:{
-        required,
-        minLength: minLength(6)
-      },
-      confirmPassword:{
-        // sameAsPassword: sameAs('password')
-        sameAsPassword: sameAs(vm => {
-          return vm.password
-          //if you want to check if its the password + b or something
-        })
-      },
-      terms: {
-        requiredUnless: requiredUnless(vm => {
-          //return true: is not required
-          //return false: is always required
-          //unless the country is china, it is required
-          return vm.country === 'china'
-        })
-      },
-      hobbyInputs: {
-        //applies to the array object
-        required,
-        minLength: minLength(2), //min number of elements
-        $each: { // applies to each of the elements
-          value: {
-            required,
-            minLength: minLength(5) //number of characters
-          }
-        }
-      }
-
+    age: {
+      required,
+      numeric,
+      minValue: minValue(18)
+      // between: between(20, 30)
     },
-    methods: {
-      onAddHobby () {
-        const newHobby = {
-          id: Math.random() * Math.random() * 1000,
-          value: ''
+    password: {
+      required,
+      minLength: minLength(6)
+    },
+    confirmPassword: {
+      // sameAsPassword: sameAs('password')
+      sameAsPassword: sameAs(vm => {
+        return vm.password;
+        //if you want to check if its the password + b or something
+      })
+    },
+    terms: {
+      requiredUnless: requiredUnless(vm => {
+        //return true: is not required
+        //return false: is always required
+        //unless the country is china, it is required
+        return vm.country === "china";
+      })
+    },
+    hobbyInputs: {
+      //applies to the array object
+      required,
+      minLength: minLength(2), //min number of elements
+      $each: {
+        // applies to each of the elements
+        value: {
+          required,
+          minLength: minLength(5) //number of characters
         }
-        this.hobbyInputs.push(newHobby)
-      },
-      onDeleteHobby (id) {
-        this.hobbyInputs = this.hobbyInputs.filter(hobby => hobby.id !== id)
-      },
-      onSubmit () {
-        const formData = {
-          email: this.email,
-          age: this.age,
-          password: this.password,
-          confirmPassword: this.confirmPassword,
-          country: this.country,
-          hobbies: this.hobbyInputs.map(h => h.value),
-          terms: this.terms
-        }
-        console.log(formData)
-        this.$store.dispatch('signup', formData)
-
-
       }
+    },
+
+  },
+  methods: {
+    onAddHobby() {
+      const newHobby = {
+        id: Math.random() * Math.random() * 1000,
+        value: ""
+      };
+      this.hobbyInputs.push(newHobby);
+    },
+    onDeleteHobby(id) {
+      this.hobbyInputs = this.hobbyInputs.filter(hobby => hobby.id !== id);
+    },
+    onSubmit() {
+      const formData = {
+        email: this.email,
+        age: this.age,
+        password: this.password,
+        confirmPassword: this.confirmPassword,
+        country: this.country,
+        hobbies: this.hobbyInputs.map(h => h.value),
+        terms: this.terms
+      };
+      console.log(formData);
+      this.$store.dispatch("signup", formData);
     }
   }
+};
 </script>
 <style scoped>
-  .text-danger{
-    color: red
-  }
-  .signup-form {
-    width: 400px;
-    margin: 30px auto;
-    border: 1px solid #eee;
-    padding: 20px;
-    box-shadow: 0 2px 3px #ccc;
-  }
+.text-danger {
+  color: red;
+}
+hr{
+ color: lightcyan;
+ border: 0.5px solid lightgray;
+ margin: 20px;
+}
+.signup-form {
+  width: 400px;
+  margin: 30px auto;
+  border: 1px solid #eee;
+  padding: 20px;
+  box-shadow: 0 2px 3px #ccc;
+}
 
-  .input {
-    margin: 10px auto;
-  }
+.input {
+  margin: 10px auto;
+}
 
-  .input label {
-    display: block;
-    color: #4e4e4e;
-    margin-bottom: 6px;
-  }
+.input label {
+  display: block;
+  color: #4e4e4e;
+  margin-bottom: 6px;
+}
 
-  .input.inline label {
-    display: inline;
-  }
+.input.inline label {
+  display: inline;
+}
 
-  .input input {
-    font: inherit;
-    width: 100%;
-    padding: 6px 12px;
-    box-sizing: border-box;
-    border: 1px solid #ccc;
-  }
+.input input {
+  font: inherit;
+  width: 100%;
+  padding: 6px 12px;
+  box-sizing: border-box;
+  border: 1px solid #ccc;
+}
 
-  .input.inline input {
-    width: auto;
-  }
+.input.inline input {
+  width: auto;
+}
 
-  .input input:focus {
-    outline: none;
-    border: 1px solid #521751;
-    background-color: #eee;
-  }
-  .input.invalid label{
-    color: red;
-  }
-  .input.invalid input{
-    border: 1px solid red;
-    background-color: #ffc9aa
-  }
+.input input:focus {
+  outline: none;
+  border: 1px solid #521751;
+  background-color: #eee;
+}
+.input.invalid label {
+  color: red;
+}
+.input.invalid input {
+  border: 1px solid red;
+  background-color: #ffc9aa;
+}
 
-  .input select {
-    border: 1px solid #ccc;
-    font: inherit;
-  }
+.input select {
+  border: 1px solid #ccc;
+  font: inherit;
+}
 
-  .hobbies button {
-    border: 1px solid #521751;
-    background: #521751;
-    color: white;
-    padding: 6px;
-    font: inherit;
-    cursor: pointer;
-  }
+.hobbies button {
+  border: 1px solid #521751;
+  background: #521751;
+  color: white;
+  padding: 6px;
+  font: inherit;
+  cursor: pointer;
+}
 
-  .hobbies button:hover,
-  .hobbies button:active {
-    background-color: #8d4288;
-  }
+.hobbies button:hover,
+.hobbies button:active {
+  background-color: #8d4288;
+}
 
-  .hobbies input {
-    width: 90%;
-  }
+.hobbies input {
+  width: 90%;
+}
 
-  .submit button {
-    border: 1px solid #521751;
-    color: #521751;
-    padding: 10px 20px;
-    font: inherit;
-    cursor: pointer;
-  }
+.submit button {
+  border: 1px solid #521751;
+  color: #521751;
+  padding: 10px 20px;
+  font: inherit;
+  cursor: pointer;
+}
 
-  .submit button:hover,
-  .submit button:active {
-    background-color: #521751;
-    color: white;
-  }
+.submit button:hover,
+.submit button:active {
+  background-color: #521751;
+  color: white;
+}
 
-  .submit button[disabled],
-  .submit button[disabled]:hover,
-  .submit button[disabled]:active {
-    border: 1px solid #ccc;
-    background-color: transparent;
-    color: #ccc;
-    cursor: not-allowed;
-  }
-
+.submit button[disabled],
+.submit button[disabled]:hover,
+.submit button[disabled]:active {
+  border: 1px solid #ccc;
+  background-color: transparent;
+  color: #ccc;
+  cursor: not-allowed;
+}
 </style>
---
+++ src/components/fields/percentage.vue
@@ -0,0 +1,184 @@
+<template>
+    <div class="input inline percentage">
+        <div class="form-group">
+            <table>
+                <tr><th colspan="2"><h3>Remaining: <span :class="{'text-danger': getSum()!==100}">{{remaining}}%</span></h3></th></tr>
+                <tr v-for="(amount, index) in amounts" :key="amounts[index].id">
+                    <th>Amount {{index + 1}}</th>
+                    <td>
+                        <div class="input" :class="{ invalide: $v.amounts.$each[index].$error}">
+                        <input class="form-control" type="number" v-model.lazy="amounts[index].value" 
+                        @blur="$v.amounts.$each[index].value.$touch()" @input="validatePercentage"/>
+                        </div>
+                        <div v-if="$v.amounts.$each[index].$error">
+                        <small v-if="!$v.amounts.$each[index].value.required" class="text-danger">Required</small>
+                        <small v-if="!$v.amounts.$each[index].value.between" class="text-danger">
+                            Must be between {{$v.amounts.$each[index].value.$params.between.min}} 
+                            and {{$v.amounts.$each[index].value.$params.between.max}}</small>
+                        </div>
+                    </td>
+                </tr>
+                    <td colspan="2">
+                    <small v-if="!$v.amounts.roundup" class="text-danger"> Must round up to 100% </small></td>
+          </table>
+        </div>      
+    </div>
+</template>
+<script>
+import { required, numeric, minValue, between } from "vuelidate/lib/validators";
+export default {
+  name: "percentage",
+  data() {
+    return {
+        amounts: [
+          { id : 'amount1', value: 0},
+          { id : 'amount2', value: 0},
+          { id : 'amount3', value: 0},
+          { id : 'amount4', value: 0},
+      ]
+    };
+  },
+  validations: {
+      amounts: {
+        required,
+        $each: {
+            value: {
+                required,
+                between: between(-100, 100) //number of characters
+            }
+        },
+        roundup(){
+            
+            return this.getSum() == 100? true : false
+        }
+      }
+  },
+  methods:{
+      getSum(){
+        return this.amounts.reduce((a,b) => { return Number(b.value) == null? Number(a) : Number(a) + Number(b.value) }, 0)
+        // return this.amounts.reduce((a,b) => { return b.value + 0 == null? a + 0 : a + b.value + 0 }, 0)
+      },
+      validatePercentage(){
+          console.log('no error? ', !this.$v.amounts.$error)
+          console.log('==100? ', this.getSum() == 100)
+          console.log('sum is: ', this.getSum())
+          if(!this.$v.amounts.$error && this.getSum() == 100){
+              this.$emit('percentageValidated');
+          }
+      }
+  },
+  computed: {
+    remaining(){
+        return 100 - this.getSum()
+    },
+  },
+  
+};
+</script>
+<style scoped>
+.text-danger {
+  color: red;
+}
+hr {
+  color: lightgray;
+  border: 0.5px solid lightgray;
+  margin: 20px;
+}
+table{
+    border: 0.5px solid lightgray;
+    margin: 20px;
+    padding: 10px;
+}
+.signup-form {
+  width: 400px;
+  margin: 30px auto;
+  border: 1px solid #eee;
+  padding: 20px;
+  box-shadow: 0 2px 3px #ccc;
+}
+
+.input {
+  margin: 10px auto;
+}
+
+.input label {
+  display: block;
+  color: #4e4e4e;
+  margin-bottom: 6px;
+}
+
+.input.inline label {
+  display: inline;
+}
+
+.input input {
+  font: inherit;
+  width: 100%;
+  padding: 6px 12px;
+  box-sizing: border-box;
+  border: 1px solid #ccc;
+}
+
+.input.inline input {
+  width: auto;
+}
+
+.input input:focus {
+  outline: none;
+  border: 1px solid #521751;
+  background-color: #eee;
+}
+.input.invalid label {
+  color: red;
+}
+.input.invalid input {
+  border: 1px solid red;
+  background-color: #ffc9aa;
+}
+
+.input select {
+  border: 1px solid #ccc;
+  font: inherit;
+}
+
+button {
+  border: 1px solid #521751;
+  background: #521751;
+  color: white;
+  padding: 6px;
+  font: inherit;
+  cursor: pointer;
+}
+
+button:hover,
+button:active {
+  background-color: #8d4288;
+}
+
+input {
+  width: 90%;
+}
+
+.submit button {
+  border: 1px solid #521751;
+  color: #521751;
+  padding: 10px 20px;
+  font: inherit;
+  cursor: pointer;
+}
+
+.submit button:hover,
+.submit button:active {
+  background-color: #521751;
+  color: white;
+}
+
+.submit button[disabled],
+.submit button[disabled]:hover,
+.submit button[disabled]:active {
+  border: 1px solid #ccc;
+  background-color: transparent;
+  color: #ccc;
+  cursor: not-allowed;
+}
+</style>
