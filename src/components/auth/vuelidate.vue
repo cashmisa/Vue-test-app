8de8f28da3e20f34f2e310484c909b28d53a7442
xiaowen <xiaowentang@kapps.com.sg>
2018-03-27 18:12:36 +0800

    some validation comparison

---
+++ src/components/auth/vuelidate.vue
@@ -0,0 +1,44 @@
+<template>
+  <div>
+    <q-input
+      v-model="form.email"
+      @blur="$v.form.email.$touch"
+      @keyup.enter="submit"
+      :error="$v.form.email.$error"
+    />
+
+    <q-btn color="primary" @click="submit">Submit</q-btn>
+  </div>
+</template>
+
+<script>
+
+
+  import { required, email } from 'vuelidate/lib/validators'
+
+  export default {
+    data () {
+      return {
+        form: {
+          email: ''
+        }
+      }
+    },
+    validations: {
+      form: {
+        email: { required, email  }
+      }
+    },
+    methods: {
+      submit () {
+        this.$v.form.$touch()
+
+        if (this.$v.form.$error) {
+          this.$q.notify('Please review fields again.')
+          return
+        }
+        // ...
+      }
+    }
+  }
+</script>
